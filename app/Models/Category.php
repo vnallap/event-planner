2804e87f05bbae1abdbc0ea4627bc0b0b9eb84b8
vnallap <v.n.nallapuneni2@wlv.ac.uk>
2025-12-30 23:22:41 +0530

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     use HasFactory;
 
     protected $connection = 'mongodb';
-    protected $collection = 'categories';
+    protected $table = 'categories';
 
     protected $fillable = [
         'name',
